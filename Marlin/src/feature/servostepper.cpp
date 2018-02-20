524e676e92f17bd92a58869a84a720cf716a5fb9
TheSFReader <TheSFReader@gmail.com>
2018-02-20 19:40:52 +0100

    Add Servostepper and reformat the stepper indirection

---
+++ Marlin/src/feature/servostepper.cpp
@@ -0,0 +1,101 @@
+/**
+ * Marlin 3D Printer Firmware
+ * Copyright (C) 2016 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ *
+ * Based on Sprinter and grbl.
+ * Copyright (C) 2011 Camiel Gubbels / Erik van der Zalm
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/**
+ * servo stepper : allow the use of a servo in place of a stepper. 
+ *
+ */
+
+
+
+#include "../inc/MarlinConfig.h"
+
+
+
+#if ENABLED(HAVE_SERVOSTEPPER)
+
+#include "servostepper.h"
+
+#include "../module/servo.h"
+
+
+
+extern Servo servo[NUM_SERVOS];
+
+ServoStepper::ServoStepper(uint8_t servoNr) {
+  servoIndex = servoNr;
+  currentPosition = 1500;
+  currentDir = 0; 
+  previousStepWrite = 0;
+  enabled = 0;
+}
+
+
+
+void ServoStepper::enable(uint8_t newenabled) {
+  if(newenabled) {
+    if(! enabled) {
+      servo[servoIndex].attach(0);
+      enabled = newenabled;
+    }
+  } else {
+    if(enabled) {
+      servo[servoIndex].detach();
+      enabled = newenabled;
+    }
+  }
+  
+}
+
+ uint8_t ServoStepper::isEnabled() {
+  return servo[servoIndex].attached();
+ }
+ 
+void ServoStepper::setDir(uint8_t direction) {
+  currentDir = direction;
+}
+
+uint8_t ServoStepper::getDir() {
+  return currentDir;
+}
+
+void ServoStepper::doStep(uint8_t stepByte) {
+  
+  if(previousStepWrite != stepByte && previousStepWrite == 0) {
+    if(currentDir) {
+      currentPosition--;
+    } else {
+      currentPosition++;
+    }
+    if(! servo[servoIndex].attached()) {
+     servo[servoIndex].attach(0);
+    }
+    servo[servoIndex].writeMicroseconds(currentPosition);
+  }
+  previousStepWrite = stepByte;
+}
+
+
+
+
+#endif // HAVE_SERVOSTEPPER
+
